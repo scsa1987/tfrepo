c7f0706e41a8e9ad375dc368a14874d9a605e58b
scsa1987 <scsa1987@gmail.com>
2024-07-31 11:14:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-provider "aws" {
-
- region = "us-east-1"
- access_key = var.access_key
- secret_key = var.secret_key
-
-
+provider "aws"{
+    region = us-east-1
+    access_key = var.access_key
+    secret_key = var.secret_key
 }
+
 
 variable "access_key " {
     type = string
@@ -14,7 +12,6 @@ variable "access_key " {
 variable "secret_key " {
     type = string
 }
-
 resource "aws_vpc" "dep1" {
 
   cidr_block = "192.168.0.0/16"
