0c58c76dd6bac3ed61418a1271f989788a7d2fb4
scsa1987 <scsa1987@gmail.com>
2024-07-31 11:36:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
 provider "aws"{
     region = "us-east-1"
-    access_key = "var.access_key"
-    secret_key = "var.secret_key"
-}
 
-
-
-variable access_key {
-    type = string
-}
-
-variable secret_key {
-    type = string
 }
 resource "aws_vpc" "dep1" {
 
